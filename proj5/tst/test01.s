afc85f8f6f095081f3fc8dbf496e04663ff55d0a
Benjamin Sanabria Carr <carrbs@cs.pdx.edu>
2012-03-10 15:14:26 -0800

    added tst/test01.s

---
+++ proj5/tst/test01.s
@@ -0,0 +1,28 @@
+	.global main
+	.align 4
+main:
+!locals=0, max_args=0
+	save %sp,-96,%sp
+! [CALLST (NAME print) ( (STRING "123"))]
+	sethi %hi(L$2),%o0
+	or %o0, %lo(L$2),%o0
+	call printf
+	nop
+! [CALLST (NAME print) ( (CONST 123))]
+	mov 123,%o1
+	sethi %hi(L$1),%o0
+	or %o0, %lo(L$1),%o0
+	call printf
+	nop
+! [CALLST (NAME print) ()]
+! [CALLST (NAME print) ( (CONST 1))]
+	mov 1,%o1
+	sethi %hi(L$1),%o0
+	or %o0, %lo(L$1),%o0
+	call printf
+	nop
+L$1:	.asciz "%d\n"
+L$2:	.asciz "123\n"
+
+!Total regs:  2
+!Total insts: 20
